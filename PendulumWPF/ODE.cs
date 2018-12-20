5d772650293ef88db81c8ff11e09c547bb052440
Aleksandr Shcherbakov <PMI71.Shcherbakov@outlook.com>
2018-12-20 10:48:15 +0700

    Add project files.

---
+++ PendulumWPF/ODE.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNumerics.ODE;
+
+namespace ODE
+{
+    public class Solution
+    {
+        public double[] t;
+        public double[] z;
+        public double[] theta;
+        public Solution(double[,] sol)
+        {
+            //sol[i,0] - t, sol[i, 1] - z, sol[i, 3] - theta
+            t = new double[sol.GetLength(0)];
+            z = new double[sol.GetLength(0)];
+            theta = new double[sol.GetLength(0)];
+            for (int i = 0; i < sol.GetLength(0); i++)
+            {
+                t[i] = sol[i, 0];
+                z[i] = sol[i, 1];
+                theta[i] = sol[i, 3];
+            }
+        }
+    }
+
+    public static class WilberforcePendulum
+    {
+        private static OdeExplicitRungeKutta45 odeRK = new OdeExplicitRungeKutta45();
+        // TODO: m, k...
+        public static Solution GetOscillations(double start_t, double delta_t, double end_t)
+        {
+            OdeFunction fun = new OdeFunction(ODEs);
+            double[] x0 = new double[4];
+            //x(i) = z, zdot, theta, thetadot
+            x0[0] = 0;
+            x0[1] = 0;
+            x0[2] = 2 * Math.PI;
+            x0[3] = 0;
+            odeRK.InitializeODEs(fun, 4);
+            double[,] sol = odeRK.Solve(x0, start_t, delta_t, end_t);
+            return new Solution(sol);
+        }
+
+        private static double[] dxdt = new double[4];
+        private static double omega = 2.314;       // rad.s-1
+        private static double epsilon = 9.27e-3;   // N
+        private static double m = 0.4905;          // kg
+        private static double I = 1.39e-4;         // kg.m2
+        private static double[] ODEs(double t, double[] x)
+        {
+            //x(i) = z, zdot, theta, thetadot
+            dxdt[0] = x[1];
+            dxdt[1] = -omega * omega * x[0] - epsilon / 2 / m * x[2];
+            dxdt[2] = x[3];
+            dxdt[3] = -omega * omega * x[2] - epsilon / 2 / I * x[0];
+            return dxdt;
+        }
+    }
+}
---
+++ PendulumWPF/Pendulum.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using HelixToolkit;
+using HelixToolkit.Wpf;
+using System.IO;
+using System.Linq;
+using System.Windows.Media.Media3D;
+using  ODE;
+
+namespace PendulumWPF
+{
+    public partial class Pendulum : Window
+    {
+        public Pendulum()
+        {
+            InitializeComponent();
+            Model3D pendulum;
+            ModelImporter importer = new ModelImporter();
+            pendulum = importer.Load("pendulum.obj");
+            ObjReader pen = new ObjReader();
+            Model3DGroup newpend = pen.Read("pendulum.obj");
+
+            our_Model.Content = newpend;
+            Create3DViewPort();
+
+        }
+
+        private void Create3DViewPort()
+        {
+            ObjReader pen = new ObjReader();
+            Model3DGroup newpend = pen.Read("pendulum.obj");
+            our_Model.Content = newpend;
+            RotateTransform3D rotate = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), 90));
+            our_Model.Transform = rotate;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new System.Windows.Window();
+            window.Content = new theta_plot();
+            window.Show();
+        }
+    }
+}
+    

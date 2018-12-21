58026bba8aa7be6730b1e0af32701cd3807cafae
Aleksandr Shcherbakov <PMI71.Shcherbakov@outlook.com>
2018-12-21 23:02:31 +0700

    working

--- PendulumWPF/ODE.cs
+++ PendulumWPF/ODE.cs
@@ -6,48 +6,47 @@ using DotNumerics.ODE;
 
 namespace ODE
 {
-    public class Solution
-    {
-        public double[] t;
-        public double[] z;
-        public double[] theta;
-        public double[] x0;
-        public Solution(double[,] sol, double[] dxdt)
-        {
-            //sol[i,0] - t, sol[i, 1] - z, sol[i, 3] - theta
-            t = new double[sol.GetLength(0)];
-            z = new double[sol.GetLength(0)];
-            theta = new double[sol.GetLength(0)];
-            for (int i = 0; i < sol.GetLength(0); i++)
-            {
-                t[i] = sol[i, 0];
-                z[i] = sol[i, 1];
-                theta[i] = sol[i, 3];
-            }
+    //public class Solution
+    //{
+    //    public double[] t;
+    //    public double[] z;
+    //    public double[] theta;
+    //    public double[] x0;
+    //    public Solution(double[,] sol, double[] dxdt)
+    //    {
+    //        //sol[i,0] - t, sol[i, 1] - z, sol[i, 3] - theta
+    //        t = new double[sol.GetLength(0)];
+    //        z = new double[sol.GetLength(0)];
+    //        theta = new double[sol.GetLength(0)];
+    //        for (int i = 0; i < sol.GetLength(0); i++)
+    //        {
+    //            t[i] = sol[i, 0];
+    //            z[i] = sol[i, 1];
+    //            theta[i] = sol[i, 3];
+    //        }
 
-            x0 = dxdt;
-        }
-    }
+    //        x0 = dxdt;
+    //    }
+    //}
 
     public static class WilberforcePendulum
     {
         private static OdeExplicitRungeKutta45 odeRK = new OdeExplicitRungeKutta45();
         // TODO: m, k...
-        public static Solution GetOscillations(double start_t, double delta_t, double end_t, double[] x0_)
+        public static double[,] GetOscillations(double start_t, double delta_t, double end_t, double[] y0)
         {
             OdeFunction fun = new OdeFunction(ODEs);
-            double[] x0 = x0_;
             //x(i) = z, zdot, theta, thetadot
-
             odeRK.InitializeODEs(fun, 4);
-            double[,] sol = odeRK.Solve(x0, start_t, delta_t, end_t);
-            return new Solution(sol, dxdt);
+            double[,] sol = odeRK.Solve(y0, start_t, delta_t, end_t);
+            return sol;
         }
 
         private static double[] dxdt = new double[4];
         private static double omega = 2.314;       // rad.s-1
         private static double epsilon = 9.27e-3;   // N
         private static double m = 0.4905;          // kg
+        //private static double m = 49.05;          // kg
         private static double I = 1.39e-4;         // kg.m2
         private static double[] ODEs(double t, double[] x)
         {
--- PendulumWPF/Pendulum.xaml.cs
+++ PendulumWPF/Pendulum.xaml.cs
@@ -17,8 +17,8 @@ namespace PendulumWPF
         {
             InitializeComponent();
             ObjReader objReader = new ObjReader();
-            Model3DGroup pendulumObject = objReader.Read("pendulum.obj");
-            scene.Content = pendulumObject;
+            //Model3DGroup pendulumObject = objReader.Read("pendulum.obj");
+            //scene.Content = pendulumObject;
             Create3DViewPort();
 
         }
@@ -26,9 +26,10 @@ namespace PendulumWPF
         private void Create3DViewPort()
         {
             ObjReader pen = new ObjReader();
-            Model3DGroup newpend = pen.Read("pendulum.obj");
-            scene.Content = newpend;
-            RotateTransform3D rotate = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), 90));
+            Model3DGroup pendulum = pen.Read("pendulum90 (3).obj");
+            scene.Content = pendulum;
+            scene.Transform = new ScaleTransform3D(2,2,2);
+            //RotateTransform3D rotate = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), 90));
             //scene.Transform = rotate;
         }
 
@@ -50,25 +51,28 @@ namespace PendulumWPF
 
         private double startT = 0;
         private double deltaT = 0.01;
-        private double endT = 0.01;
-        private double[] x0 = new double[] {0, 0, Math.PI * 2, 0 };
+        private double endT = 0.02;
+        private double[] y0 = new double[] {0, 0, Math.PI * 2, 0 };
         private Transform3DGroup transform;
         private RotateTransform3D rotate;
         private TranslateTransform3D translate;
         private double i = 0;
         private void timerTick(object sender, EventArgs e)
         {
-            var solve = WilberforcePendulum.GetOscillations(startT, deltaT, endT, x0);
-            x0 = solve.x0;
+            //solve[i,0] - t, solve[i, 1] - z, solve[i, 2] - zdot, solve[i, 3] - theta, solve[i, 4] - thetadot 
+            double[,] solve = WilberforcePendulum.GetOscillations(startT, deltaT, endT, y0);
+            y0 = new[] {solve[1, 1], solve[1, 2], solve[1, 3], solve[1, 4]};
+         
             startT += deltaT;
             endT += deltaT;
-            rotate = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), solve.theta[0]*180 / Math.PI));
-            //translate = new TranslateTransform3D(new Vector3D(0,0,solve.z[0]));
+            rotate = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), solve[0, 3] * 180 / Math.PI));
+            translate = new TranslateTransform3D(0,0,20*solve[0, 1]);
             transform = new Transform3DGroup();
             transform.Children.Add(rotate);
-            //transform.Children.Add(translate);
+            transform.Children.Add(translate);
+            transform.Children.Add(new ScaleTransform3D(2, 2, 2));
             scene.Transform = transform;
-            Console.WriteLine(solve.theta[0]);
+            Console.WriteLine(solve[0, 1]);
         }
 
     }
